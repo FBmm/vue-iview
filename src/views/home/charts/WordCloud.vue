7a6cc457907cc560216bd9671a3dc0e77d421454
wu_mac <wuq_xd@163.com>
2020-11-10 00:57:07 +0800

    词云图实现

---
+++ src/views/home/charts/WordCloud.vue
@@ -0,0 +1,159 @@
+<template>
+  <Card dis-hover>
+    <p slot="title">前端关键词</p>
+    <div id="WordCloud" style="height: 300px;"></div>
+  </Card>
+</template>
+
+<script>
+import echarts from "echarts";
+import 'echarts-wordcloud';
+export default {
+  data() {
+    return {
+      chart: null
+    };
+  },
+  mounted() {
+    // 基于准备好的dom，初始化echarts实例
+    this.chart = echarts.init(document.getElementById("WordCloud"));
+    this.chart.setOption(this.getOption());
+  },
+  methods: {
+    getOption() {
+      let data = [];
+      for (let i = 0; i < 5; i++) {
+        const list = [
+          {
+            name: "vuex",
+            value: 1898
+          },
+          {
+            name: "vue-router",
+            value: 1484
+          },
+          {
+            name: "Express",
+            value: 1112
+          },
+          {
+            name: "Nodejs",
+            value: 965
+          },
+          {
+            name: "npm",
+            value: 847
+          },
+          {
+            name: "git",
+            value: 582
+          },
+          {
+            name: "javascript",
+            value: 555
+          },
+          {
+            name: "Typescript",
+            value: 550
+          },
+          {
+            name: "CSS世界",
+            value: 462
+          },
+          {
+            name: "js",
+            value: 366
+          },
+          {
+            name: "模块化",
+            value: 360
+          },
+          {
+            name: "scss",
+            value: 282
+          },
+          {
+            name: "echarts",
+            value: 273
+          },
+          {
+            name: "组件化",
+            value: 265
+          }
+        ];
+        data = data.concat(list);
+      }
+      return {
+        tooltip: {},
+        series: [
+          {
+            type: "wordCloud",
+            gridSize: 2,
+            sizeRange: [12, 50],
+            rotationRange: [-90, 90],
+            shape: "pentagon",
+            width: 600,
+            height: 400,
+            textStyle: {
+              normal: {
+                color: function() {
+                  return (
+                    "rgb(" +
+                    [
+                      Math.round(Math.random() * 160),
+                      Math.round(Math.random() * 160),
+                      Math.round(Math.random() * 160)
+                    ].join(",") +
+                    ")"
+                  );
+                }
+              },
+              emphasis: {
+                shadowBlur: 10,
+                shadowColor: "#333"
+              }
+            },
+            data: [
+              {
+                name: "Vue",
+                value: 10000,
+                textStyle: {
+                  normal: {
+                    color: "black"
+                  },
+                  emphasis: {
+                    color: "red"
+                  }
+                }
+              },
+              {
+                name: "React",
+                value: 6181
+              },
+              {
+                name: "HTML5",
+                value: 4386
+              },
+              {
+                name: "CSS3",
+                value: 4055
+              },
+              {
+                name: "Webpack",
+                value: 2467
+              },
+              {
+                name: "Angular",
+                value: 2244
+              }
+            ].concat(data)
+          }
+        ]
+      };
+    }
+  }
+};
+</script>
+
+<style>
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -5,6 +5,9 @@
       <PageViewChart1 />
       <PageViewChart2 />
     </div>
+    <div class="three-layout">
+      <WordCloud />
+    </div>
   </div>
 </template>
 
@@ -12,8 +15,9 @@
 import PageViewChart from "./charts/PageView"
 import PageViewChart1 from "./charts/PageView1"
 import PageViewChart2 from "./charts/PageView2"
+import WordCloud from "./charts/WordCloud"
 export default {
-  components: { PageViewChart, PageViewChart1, PageViewChart2 }
+  components: { PageViewChart, PageViewChart1, PageViewChart2, WordCloud }
 }
 </script>
 
@@ -22,6 +26,12 @@ export default {
   margin-top: 24px;
   display: grid;
   grid-template-columns: 2fr 1fr;
+  grid-gap: 24px;
+}
+.three-layout {
+  margin-top: 24px;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
   grid-gap: 24px;
 }
 </style>

616cbb62e1baa41b14e1fb6752aba9be628e6174
fbm <wuq_xd@163.com>
2020-12-10 21:19:01 +0800

    测试页面、build配置

--- src/views/test/index.vue
+++ src/views/test/index.vue
@@ -5,22 +5,41 @@
       <template #content="scopeData">{{content}}{{scopeData}}</template>
       <template #footer>底部</template>
     </Header>
+    <Button type="error" @click="onClick">这是一个按钮</Button>
   </div>
 </template>
 
 <script>
 import Header from "./header"
+import { debounce } from "lodash" // eslint-disable-line
+import { throttle } from "lodash" // eslint-disable-line
+import { throttle as th, debounce as de } from "throttle-debounce" // eslint-disable-line
 export default {
   name: "test",
   components: { Header },
   data() {
     return {
       name: "测试",
-      content: "内容"
+      content: "内容",
+      count: 0,
     }
   },
   mounted() {
     console.log(this)
+    this.fn = debounce(this.print, 1000, {
+      leading: false,
+      trailing: true,
+    })
+    // this.fn = de(1000, false, this.print)
+  },
+  methods: {
+    print(a) {
+      console.log(a)
+    },
+    onClick() {
+      this.count++
+      this.fn(this.count)
+    }
   }
 }
 </script>
